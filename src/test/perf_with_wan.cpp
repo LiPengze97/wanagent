cb9fe5b267bae13367211d25779e8a0743eb8d7e
equ-0 <zijianding@163.com>
2021-04-09 02:36:36 +0800

    small update

--- src/test/perf_with_wan.cpp
+++ src/test/perf_with_wan.cpp
@@ -41,7 +41,7 @@ static void print_help(const char* cmd) {
 
 #define MAX_SEND_BUFFER_SIZE (102400)
 #define SLEEP_GRANULARITY_US (50)
-#define expected_mps (500)
+#define expected_mps (300)
 
 const int MAXOPS = 1e5 + 100;
 
