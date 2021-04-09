7d2bc0ba93d15d0a4e7d784f6e8d2a44951c6d43
equ-0 <zijianding@163.com>
2021-04-09 19:48:28 +0800

    u

--- src/impl/wan_agent_impl.cpp
+++ src/impl/wan_agent_impl.cpp
@@ -364,6 +364,7 @@ void MessageSender::wait_read_predicate(const uint64_t seq,
     if (read_stability_frontier > seq) {
         if (seq % 5000 == 0) 
             std::cerr << seq << ' ' << site << std::endl;
+        assert(read_callback_store[seq] != nullptr);
         (*(read_callback_store[seq]))(version, site, std::move(obj));
         read_callback_store.erase(read_callback_store.find(seq));
         disregards[seq] = 1;
