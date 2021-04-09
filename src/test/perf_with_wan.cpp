614b2b025d898434112ff7d9939382ef91f84bfd
equ-0 <zijianding@163.com>
2021-04-09 20:31:43 +0800

    p

--- src/test/perf_with_wan.cpp
+++ src/test/perf_with_wan.cpp
@@ -265,11 +265,11 @@ int main(int argc, char** argv) {
     while (T_fin >> ops) {
         ++trans_ctr;
         if (trans_ctr % 1000 == 0) cerr << trans_ctr << endl;
-        now_time = now_us();
-        while ((now_time - start_time)/1000000.0*expected_mps < (trans_ctr - 1)) {
-            std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_GRANULARITY_US));
-            now_time = now_us();
-        }
+        // now_time = now_us();
+        // while ((now_time - start_time)/1000000.0*expected_mps < (trans_ctr - 1)) {
+        //     std::this_thread::sleep_for(std::chrono::microseconds(SLEEP_GRANULARITY_US));
+        //     now_time = now_us();
+        // }
         if (ops[0] == 'R') {
             T_fin >> version;
             ++read_ctr;
