0cdde0032b7d0c41a415c32cb54ceb55d6646fcc
equ_cd7e <zijianding@163.com>
2021-04-10 15:59:09 +0800

    a

--- src/impl/wan_agent_impl.cpp
+++ src/impl/wan_agent_impl.cpp
@@ -456,7 +456,7 @@ void MessageSender::recv_read_ack_loop() {
                 read_message_counters[res.site_id]++;
                 auto pr_sf = read_stability_frontier;
                 read_predicate_calculation();
-                // std::cout << "current read stability frontier = " + std::to_string(read_stability_frontier) + '\n';
+                std::cerr << "current read stability frontier = " + std::to_string(read_stability_frontier) + '\n';
                 if (read_stability_frontier > pr_sf)
                     trigger_read_callback(res.seq, cur_version, res.site_id, std::move(cur_obj));
             }
