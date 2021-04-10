3445ae8ddb758d2ec4acb44524abac9e87cff559
equ_cd7e <zijianding@163.com>
2021-04-10 14:53:30 +0800

    final

--- src/test/perf_with_wan.cpp
+++ src/test/perf_with_wan.cpp
@@ -273,8 +273,8 @@ int main(int argc, char** argv) {
         check_out(read_ctr, write_ctr, SWI ? w_name[T] : r_name[T]);
         wan_agent_sender.wait();
     }
-    std::cerr << "hi" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    wan_agent_sender.shutdown_and_wait();
+//    std::cerr << "hi" << std::endl;
+//    std::this_thread::sleep_for(std::chrono::seconds(1));
+//    wan_agent_sender.shutdown_and_wait();
     free(time_keeper);
 }
